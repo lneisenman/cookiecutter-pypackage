d12134d1543cebc2d25f866722872020cc6ff7b7
Larry Eisenman <leisenman@wustl.edu>
2017-02-27 12:02:40 -0600

    forgot . in front of import file

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
@@ -3,7 +3,7 @@
 from __future__ import (print_function, division, absolute_import,
                         unicode_literals)
 
-from {{ cookiecutter.project_slug }} import {{ cookiecutter.project_slug }}_fcn
+from .{{ cookiecutter.project_slug }} import {{ cookiecutter.project_slug }}_fcn
 
 __author__ = """{{ cookiecutter.full_name }}"""
 __email__ = '{{ cookiecutter.email }}'
